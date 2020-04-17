21f63538be7c4dcedc1900e695397b0749353dcd
adityaekaprams <63759425+adityaekaprams@users.noreply.github.com>
2020-04-17 17:06:21 +0700

    Add files via upload

---
+++ aset/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="http://localhost/siperpus/aset/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="http://localhost/siperpus/aset/fontawesome/css/all.min.css">
+  <title>SiPERPUS</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#"><i class="fas fa-book-reader text-white mr-2"> SIPERPUS || HY ADMIN</i></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+        <a class="nav-item nav-link" href="http://localhost/siperpus/index.php">Dashboard</a>
+        <a class="nav-item nav-link" href="http://localhost/siperpus/buku/index.php">Buku</a>
+        <a class="nav-item nav-link" href="http://localhost/siperpus/anggota/index.php">Anggota</a>
+        <a class="nav-item nav-link" href="http://localhost/siperpus/transaksi/index.php">peminjaman</a>
+        <a class="nav-item nav-link" href="#">logout</a>
+      </li>
+    </ul>
+  </div>
+</nav>
